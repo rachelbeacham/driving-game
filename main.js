49293f80abe088734ef182f985fcd2639c725860
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-05 14:46:41 -0800

    Completed issue 2, user can turn car with arrow key.#2

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+var $car = document.querySelector('.car')
+
 var data = {
-  direction: 'east',
+  direction: 'east'
 }
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', turnCar)
 
-});
+function turnCar(e) {
+  var keyPressed = e.key;
+  if (keyPressed === 'ArrowUp') {
+    $car.className = 'car north';
+    data.direction = 'north'
+  } else if (keyPressed ==='ArrowLeft') {
+    $car.className = 'car west';
+    data.direction = 'west';
+  } else if (keyPressed === 'ArrowRight') {
+    $car.className = 'car';
+    data.direction = 'east';
+  } else if (keyPressed === 'ArrowDown') {
+    $car.className = 'car south';
+    data.direction = 'south';
+  } else {
+    return;
+  }
+ }
